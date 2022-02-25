9b49818cb8006a45384249cc2250c661cebaeb7b
Jensen <jensenbeard00@gmail.com>
2022-02-25 14:53:19 +0000

    Added Basic alarm Implementation (Backup if services Fail)

--- app/src/main/java/com/example/paimasapp/activities/TestActivity.kt
+++ app/src/main/java/com/example/paimasapp/activities/TestActivity.kt
@@ -2,21 +2,50 @@ package com.example.paimasapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextClock
 import android.widget.TextView
+import android.widget.TimePicker
 import com.example.paimasapp.R
 import java.text.SimpleDateFormat
 import java.util.*
 
 class TestActivity : AppCompatActivity() {
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.alarm_picker_fragment)
 
-        val timeText: TextView = findViewById((R.id.textView))
-        val sdf = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
-        val currentDate = sdf.format(Date())
-        timeText.setText(currentDate)
-
+       /* val timePicker: TimePicker = findViewById(R.id.timePicker)
+        val textClock: TextClock = findViewById(R.id.textClock)
+        val timer = Timer()
+        timer.scheduleAtFixedRate(object : TimerTask() {
+            override fun run() {
+                if(textClock.text.toString() == alarmTime(timePicker)){
+                    Log.d("Alarm","This Is An Alarm")
+                }
+            }
+        }, 0, 1000)*/
     }
 
+   /* private fun alarmTime(timePicker : TimePicker): String {
+        var alarmHour : Int = timePicker.hour
+        var alarmMinutes : Int = timePicker.minute
+        var stringAlarmTime : String = ""
+
+        if(alarmHour>12){
+            alarmHour -= 12
+            stringAlarmTime = "$alarmHour:$alarmMinutes PM"
+        } else {
+            stringAlarmTime = "$alarmHour:$alarmMinutes AM"
+        }
+
+        return stringAlarmTime
+
+
+    }*/
 }
