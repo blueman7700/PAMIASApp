f38841bde9f8fa8ed4da4aac1cb22f5b1d069c89
Jensen <jensenbeard00@gmail.com>
2022-02-25 11:39:34 +0000

    Added Current time and page switching intent

--- app/src/main/java/com/example/paimasapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/paimasapp/activities/MainActivity.kt
@@ -1,14 +1,26 @@
 package com.example.paimasapp.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.paimasapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        val button = findViewById<Button>(R.id.setAlarm)
+        button.setOnClickListener{
+            val intent = Intent(this, TestActivity::class.java
+            )
+            startActivity(intent)
+        }
     }
+
 
 
 }
--- app/src/main/java/com/example/paimasapp/activities/TestActivity.kt
+++ app/src/main/java/com/example/paimasapp/activities/TestActivity.kt
@@ -2,11 +2,21 @@ package com.example.paimasapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.example.paimasapp.R
+import java.text.SimpleDateFormat
+import java.util.*
 
 class TestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_test)
+        setContentView(R.layout.alarm_picker_fragment)
+
+        val timeText: TextView = findViewById((R.id.textView))
+        val sdf = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
+        val currentDate = sdf.format(Date())
+        timeText.setText(currentDate)
+
     }
+
 }
