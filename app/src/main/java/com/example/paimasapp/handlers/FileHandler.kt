244ed98737238ab6887ecd2010ae719772e4898d
Oliver Morris <blueman7700@gmail.com>
2022-03-11 09:35:13 +0000

    Created initial files for a card holder activity and a static file handler

---
+++ app/src/main/java/com/example/paimasapp/handlers/FileHandler.kt
@@ -0,0 +1,20 @@
+package com.example.paimasapp.handlers
+
+class FileHandler {
+
+    //FUN FACT: this is the only way to make static classes in kotlin :)
+    companion object {
+
+        //TODO: add file reading/writing here
+
+        @JvmStatic
+        fun readFile(path: String) {
+
+        }
+
+        @JvmStatic
+        fun writeFile(path: String) {
+
+        }
+    }
+}
