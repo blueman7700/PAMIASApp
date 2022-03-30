8efdd5844be10784435bacf0cb5b483e5cc4ad41
Jensen <jensenbeard00@gmail.com>
2022-03-30 17:06:57 +0100

    added initial code for sensor frag

--- app/src/main/java/com/example/paimasapp/ui/fragments/SensorInteractionFragment.kt
+++ app/src/main/java/com/example/paimasapp/ui/fragments/SensorInteractionFragment.kt
@@ -1,26 +1,39 @@
 package com.example.paimasapp.ui.fragments
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.SystemClock
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.paimasapp.R
+import com.phidget22.PhidgetException
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
+
+
 /**
  * A simple [Fragment] subclass.
  * Use the [SensorInteractionFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SensorInteractionFragment : Fragment() {
+class SensorInteractionFragment : Fragment(), SensorEventListener {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var mSensorManager: SensorManager
+    var mSensor: Sensor? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +41,7 @@ class SensorInteractionFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
     }
 
     override fun onCreateView(
@@ -56,5 +70,43 @@ class SensorInteractionFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        mSensorManager = activity?.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        mSensor = mSensorManager!!.getDefaultSensor(Sensor.TYPE_GEOMAGNETIC_ROTATION_VECTOR)
+
+    }
+    var dtOld = SystemClock.elapsedRealtimeNanos()
+    override fun onSensorChanged(p0: SensorEvent?) {
+        if (p0 != null) {
+            if (p0.sensor.getType() == Sensor.TYPE_GEOMAGNETIC_ROTATION_VECTOR) {
+                /*xTextView.setText("X = " + String.format("%.3f", event.values.get(0)))
+                yTextView.setText("Y = " + String.format("%.3f", event.values.get(1)))
+                zTextView.setText("Z = " + String.format("%.3f", event.values.get(2)))
+                aTextView.setText("A = " + String.format("%.3f", event.values.get(3)))*/
+                try {
+                    val gyro: Float = p0.values[1]
+                    var dt: Double = (p0.timestamp - dtOld).toDouble()
+                    dt /= 1000000.0
+                    val angle: Double = +gyro * dt
+                    val acc_data: Float = p0.values[3]
+                    dtOld = p0.timestamp
+                    val targetPos = 0.98 * angle + 0.02 * acc_data
+                    Log.d("Test", targetPos.toString())
+
+
+                } catch (phidgetException: PhidgetException) {
+                    phidgetException.printStackTrace()
+                }
+            }
+        }
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+        TODO("Not yet implemented")
     }
 }
