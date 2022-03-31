4617272e59152bf5c5f5a42e3d8463154b93ee69
Jensen <jensenbeard00@gmail.com>
2022-03-31 12:47:59 +0100

    Finalised sensor event with broadcast

--- app/src/main/java/com/example/paimasapp/ui/fragments/SensorInteractionFragment.kt
+++ app/src/main/java/com/example/paimasapp/ui/fragments/SensorInteractionFragment.kt
@@ -1,6 +1,7 @@
 package com.example.paimasapp.ui.fragments
 
 import android.content.Context
+import android.content.Intent
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
@@ -14,14 +15,12 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.paimasapp.R
-import com.phidget22.PhidgetException
+import java.util.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
-
-
 
 /**
  * A simple [Fragment] subclass.
@@ -34,7 +33,7 @@ class SensorInteractionFragment : Fragment(), SensorEventListener {
     private var param2: String? = null
     private lateinit var mSensorManager: SensorManager
     var mSensor: Sensor? = null
-
+    var targetVal: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -78,27 +77,55 @@ class SensorInteractionFragment : Fragment(), SensorEventListener {
 
         //I dont think this is attacking properly
         mSensorManager = this.requireActivity().getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        mSensor = mSensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        mSensor = mSensorManager!!.getDefaultSensor(Sensor.TYPE_GEOMAGNETIC_ROTATION_VECTOR)
+        mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL)
         Log.d("nullCheck","SensorManager created?")
 
+        targetVal = Random().nextInt(68) - 34
 
+        val taskText = "Rotate your device in the Z axis to $targetVal"
+        val tText = this.requireView().findViewById<TextView>(R.id.taskText)
+        tText.text = taskText
     }
-
+    private var dtOld = SystemClock.elapsedRealtimeNanos().toDouble()
     override fun onSensorChanged(p0: SensorEvent?) {
         if (p0 != null) {
-            if(p0.sensor.type == Sensor.TYPE_ACCELEROMETER){
-                this.requireActivity().findViewById<TextView>(R.id.accelText).text =
-                    p0.values.toString()
+            if(p0.sensor.type == Sensor.TYPE_GEOMAGNETIC_ROTATION_VECTOR){
 
-            } else {
-                Log.d("nullCheck","Sensor is null")
+                val gyro: Float = p0.values[2]
+                var dt: Double = (p0.timestamp - dtOld)
+                dt /= 1000000.0
+                val angle: Double = (+gyro * dt).toDouble()
+                val acc_data: Float = p0.values[3]
+                dtOld = p0.timestamp.toDouble()
+
+                val targetPos = 0.98 * angle + 0.02 * acc_data
+
+                this.requireActivity().findViewById<TextView>(R.id.accelText).text = "Cur X: " +
+                        targetPos.toInt().toString()
+                if(targetPos.toInt() == targetVal){
+                    Log.d("Trigger","Target threshold met")
+
+                    val intent = Intent()
+                    intent.action = "sensor"
+                    this.requireActivity().sendBroadcast(intent)
+                }
             }
-        } else {
-            Log.d("nullCheck","SensorEvent is null")
         }
     }
 
     override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
-        TODO("Not yet implemented")
+        return
     }
+
+    override fun onResume() {
+        super.onResume()
+
+    }
+    override fun onPause() {
+        super.onPause()
+       // mSensorManager.unregisterListener(this)
+    }
+
+
 }
